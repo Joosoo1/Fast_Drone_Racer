d97b90d88ba958a4f60069206c2b30e57905baf5
joosoo <2099234036@qq.com>
2024-04-18 10:20:06 +0800

    fix

--- fast250_sh/run_ov.sh
+++ fast250_sh/run_ov.sh
@@ -1,4 +1,6 @@
 roslaunch realsense2_camera rs_camera.launch & sleep 10;
 roslaunch mavros px4.launch & sleep 10;
+rosrun mavros mavcmd long 511 105 5000 0 0 0 0 0 & sleep 1;
+rosrun mavros mavcmd long 511 31 5000 0 0 0 0 0 & sleep 1;
 roslaunch ov_msckf subscribe_fast250.launch
 wait;
